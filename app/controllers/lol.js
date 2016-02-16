674c49ac16ec227f9b1900dffc9412f763eac1fd
Andrius Petrauskis <stexaz@gmail.com>
2016-02-16 16:42:41 +0200

    Update parsing of user name - now allowing spaces in name

--- app/controllers/lol.js
+++ app/controllers/lol.js
@@ -11,7 +11,11 @@ module.exports = {
 
             switch (parts[0]) {
                 case 'link':
-                    lol.link(parts[1], req.body.user_name, parts[2]);
+                    //user name can have spaces
+                    var user = parts;
+                    delete user[0];
+                    delete user[1];
+                    lol.link(parts[1], req.body.user_name, user.join(' '));
                     break;
                 case 'status':
                     lol.status(req.body.user_name, parts[1]);
