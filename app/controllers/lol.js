68cd636f60037d562be0cf70d55f2877656c1349
Andrius Petrauskis <stexaz@gmail.com>
2016-02-16 17:39:27 +0200

    Improve username with spaces stuff

--- app/controllers/lol.js
+++ app/controllers/lol.js
@@ -12,10 +12,7 @@ module.exports = {
             switch (parts[0]) {
                 case 'link':
                     //user name can have spaces
-                    var user = parts;
-                    delete user[0];
-                    delete user[1];
-                    lol.link(parts[1], req.body.user_name, user.join(' '));
+                    lol.link(parts[1], req.body.user_name, parts.slice(2).join(' '));
                     break;
                 case 'status':
                     lol.status(req.body.user_name, parts[1]);
