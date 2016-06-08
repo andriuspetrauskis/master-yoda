dc7bf0db36a57fe79d92aab26a2458a10a372fe1
Andrius Petrauskis <stexaz@gmail.com>
2016-06-08 23:51:45 +0300

    Implement recent user check

--- app/controllers/recent.js
+++ app/controllers/recent.js
@@ -1,7 +1,24 @@
 'use strict';
 
+var repo = require('../repositories/lol.js');
+
 module.exports = {
     indexAction: function (req, res) {
-
+        repo.getJoinedSince(parseInt(req.get('since'))).then(
+            function (users) {
+                return users.map(function (user) {
+                    return {
+                        'joined': user.joined,
+                        'lastGame': user.summoners.reduce(function (prev, curr) {
+                            return prev.lastGame > curr.lastGame ? prev : curr;
+                        }).lastGame
+                    };
+                });
+            }
+        ).then(
+            function (result) {
+                res.jsonp(result);
+            }
+        );
     }
 };
