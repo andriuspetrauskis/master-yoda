6b7cf10ef0546994031c34a67b0c310f8e6efda4
Andrius Petrauskis <stexaz@gmail.com>
2016-06-08 22:43:19 +0300

    Add recent path & controller

---
+++ app/controllers/recent.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    indexAction: function (req, res) {
+
+    }
+};
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var yoda = require('../controllers/lol.js');
+var recent = require('../controllers/recent.js');
 
 module.exports = function(app) {
     app.post('/api/lol', yoda.lolAction);
+};
+module.exports = function(app) {
+    app.post('/api/recent', recent.indexAction);
 };
