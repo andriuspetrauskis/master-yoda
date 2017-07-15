57fd203807425f78440fe7e52a36c8d6f70f3dc1
Andrius Petrauskis <stexaz@gmail.com>
2017-07-16 01:40:16 +0300

    Introduce new API version adapter

---
+++ app/adapters/riotgames/lol.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// this is adapter for v3 riot API
+
+var rp = require('request-promise');
+
+module.exports = {
+    'byName' : function (region, name) {
+        return rp(this.getServerUri(region) + 'summoner/v3/summoners/by-name/' + name + this.getApiKey())
+            .then(function (response) {
+                return JSON.parse(response);
+            });
+    },
+    'getIdByName': function (region, name) {
+        return this.byName(region, name).then(function (data) {
+            return data.accountId;
+        });
+    },
+    'lastMatches': function (region, accountId) {
+        return rp(this.getServerUri(region) + 'match/v3/matchlists/by-account/' + accountId + '/recent' + this.getApiKey())
+            .then(function (response) {
+                return JSON.parse(response);
+            });
+    },
+    'lastMatch': function (region, accountId) {
+        return this.lastMatches(region, accountId).then(function (matches) {
+            return matches[0];
+        });
+    },
+    'getServerUri': function (region) {
+        if ('undefined' === typeof this.regions[region]) {
+            throw new Error("Invalid region");
+        }
+
+        return 'https://' + this.regions[region] + '.api.riotgames.com/lol/';
+    },
+    'getApiKey': function () {
+        return '?api_key=' + process.env.API_KEY;
+    },
+    regions: {
+        'br': 'br1',
+        'eune': 'eun1',
+        'euw': 'euw1',
+        'jp': 'jp1',
+        'kr': 'kr',
+        'lan': 'la1',
+        'las': 'la2',
+        'na': 'na',
+        'na1': 'na1',
+        'oce': 'oc1',
+        'tr': 'tr1',
+        'ru': 'ru',
+        'pbe': 'pbe1'
+    }
+};
